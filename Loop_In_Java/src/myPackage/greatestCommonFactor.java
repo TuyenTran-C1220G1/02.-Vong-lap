c5aaeacc2c676fbdacd5b3febe35e16e9629aa77
tduyen.86@gmail.com <tduyen.86@gmail.com>
2021-02-05 13:34:14 +0700

    done

---
+++ Loop_In_Java/src/myPackage/greatestCommonFactor.java
@@ -0,0 +1,33 @@
+package myPackage;
+
+import java.util.Scanner;
+
+public class greatestCommonFactor {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a;
+        int b;
+
+        System.out.println("Enter a: ");
+        a = scanner.nextInt();
+
+        System.out.println("Enter b: ");
+        b = scanner.nextInt();
+
+        a = Math.abs(a);
+        b = Math.abs(b);
+
+        if (a == 0 || b == 0) {
+            System.out.println("no greatest common factor");
+        }
+
+        while (a != b) {
+            if (a > b) {
+                a = a - b;
+            } else {
+                b = b - a;
+            }
+        }
+        System.out.println(" Greatest common factor: " + a);
+    }
+}
